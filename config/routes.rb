dc929a49b077fe65e1092b22db91d4074e628658
Taylor <taylor@crucialartists.com>
2023-01-13 15:11:31 -0600

    Add sessions controller and route

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   resources :employers
   resources :assistants
   resources :accounts
+  post "/login", to: "sessions#create"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
