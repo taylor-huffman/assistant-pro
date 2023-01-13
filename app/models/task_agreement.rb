ba3900292707ce398d1e12bafe54c55b118b6a69
Taylor <taylor@crucialartists.com>
2023-01-13 14:48:46 -0600

    Modify task_agreement resource

--- app/models/task_agreement.rb
+++ app/models/task_agreement.rb
@@ -1,5 +1,6 @@
 class TaskAgreement < ApplicationRecord
     belongs_to :assistant
+    belongs_to :employer
     belongs_to :task_post
     has_one :review
 end
--- app/serializers/task_agreement_serializer.rb
+++ app/serializers/task_agreement_serializer.rb
@@ -1,7 +1,8 @@
 class TaskAgreementSerializer < ActiveModel::Serializer
-  attributes :id, :hourly_rate, :task_agreement_notes, :is_completed, :task_post_id, :assistant, :review, :created_at, :updated_at
+  attributes :id, :hourly_rate, :task_agreement_notes, :is_completed, :task_post, :assistant, :review, :created_at, :updated_at
 
   belongs_to :assistant
+  belongs_to :employer
   belongs_to :task_post
   has_one :review
 end
