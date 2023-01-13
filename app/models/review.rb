e8c3dd37f80b994e203e779ef198d9bc4438c276
Taylor <taylor@crucialartists.com>
2023-01-13 14:48:00 -0600

    Modify review resource

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ApplicationRecord
     belongs_to :assistant
+    belongs_to :employer
     belongs_to :task_agreement
+    has_one :task_post, through: :task_agreement
 end
--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,6 +1,8 @@
 class ReviewSerializer < ActiveModel::Serializer
-  attributes :id, :review, :rating, :task_agreement
+  attributes :id, :review_text, :rating, :task_agreement, :task_post
 
   belongs_to :assistant
+  belongs_to :employer
   belongs_to :task_agreement
+  has_one :task_post, through: :task_agreement
 end
