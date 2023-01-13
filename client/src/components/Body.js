ea48bfbf03ae1f9031555dc0c95c1e3c80b4917b
Taylor <taylor@crucialartists.com>
2023-01-13 17:03:02 -0600

    Create frontend routes for home, profileemployer and account

--- client/src/components/Body.js
+++ client/src/components/Body.js
@@ -1,14 +1,25 @@
 import React from 'react'
-// import Account from './Account'
-// import Home from './Home'
+import Account from './Account'
+import Home from './Home'
 import ProfileEmployer from './ProfileEmployer'
+import { Route, Switch } from 'react-router-dom'
 
 
 function Body() {
 
     return (
         <>
-            <ProfileEmployer/>
+            <Switch>
+                <Route exact path="/account">
+                    <Account/>
+                </Route>
+                <Route exact path="/account/profile-employer">
+                    <ProfileEmployer/>
+                </Route>
+                <Route exact path="/">
+                    <Home/>
+                </Route>
+            </Switch>
         </>
     )
 }
