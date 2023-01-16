4d5842dfa6a7ac0e7cc87c7b138028af99646e81
Taylor <taylor@crucialartists.com>
2023-01-16 09:55:50 -0600

    Change password column to password_digest

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_14_194152) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_14_222337) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "accounts", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.string "address"
     t.string "phone"
     t.datetime "created_at", null: false
