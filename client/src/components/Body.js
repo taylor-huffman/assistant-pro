c207cc5413970851f2e414c36319e0f16aa31a0a
Taylor <taylor@crucialartists.com>
2023-01-26 14:00:08 -0600

    Bring in user context and attempt to setup redirects in routes

--- client/src/components/Body.js
+++ client/src/components/Body.js
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Account from './Account'
 import Home from './Home'
 import ProfileEmployer from './ProfileEmployer'
+import ProfileAssistant from './ProfileAssistant'
 import Login from './Login'
 import { Route, Switch } from 'react-router-dom'
+import { UserContext } from '../context/user';
+import { Redirect } from 'react-router-dom'
 
 
 function Body() {
+
+    const { user } = useContext(UserContext)
 
     return (
         <>
@@ -15,10 +20,16 @@ function Body() {
                     <Login/>
                 </Route>
                 <Route exact path="/account">
+                    {/* {!isAuth ? <Redirect to="/login" /> : <Account/>} */}
                     <Account/>
                 </Route>
                 <Route exact path="/account/profile-employer">
+                    {/* {user.error ? <Redirect to="/login" /> : <ProfileEmployer/>} */}
                     <ProfileEmployer/>
+                </Route>
+                <Route exact path="/account/profile-assistant">
+                    {/* {user.error ? <Redirect to="/login" /> : <ProfileAssistant/>} */}
+                    <ProfileAssistant/>
                 </Route>
                 <Route exact path="/">
                     <Home/>
