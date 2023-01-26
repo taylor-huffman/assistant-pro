a1b54a03f13917343c4bfe0cb547a2591ded04ea
Taylor <taylor@crucialartists.com>
2023-01-26 14:06:35 -0600

    Add has_secure_password and validates for email

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,6 +1,10 @@
 class Account < ApplicationRecord
+    has_secure_password
+
     has_one :assistant
     has_one :employer
     has_many :task_posts, through: :employer
     # has_many :task_agreements, through: :task_posts
+
+    validates :email, presence: true, uniqueness: true
 end
