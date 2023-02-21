61b8ed49566b666dd737e1ca712d9334f01a35ec
Taylor <taylor@crucialartists.com>
2023-02-21 08:29:45 -0600

    Change associations to has_one and add dependent destroy to review

--- app/models/task_agreement.rb
+++ app/models/task_agreement.rb
@@ -2,5 +2,6 @@ class TaskAgreement < ApplicationRecord
     belongs_to :assistant
     belongs_to :employer
     belongs_to :task_post
-    has_one :review
+    has_one :task_category, through: :task_post
+    has_one :review, dependent: :destroy
 end
