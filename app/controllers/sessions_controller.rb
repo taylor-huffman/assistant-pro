c3330b6b69e986fd125f722368881a469a30dff1
Taylor <taylor@crucialartists.com>
2023-02-21 08:23:38 -0600

    Add destroy and private methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,8 @@
 class SessionsController < ApplicationController
+rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+before_action :authorize
+skip_before_action :authorize, only: [:create, :destroy]
+
     def create
         account = Account.find_by(email: params[:email])
         if account&.authenticate(params[:password])
@@ -7,5 +11,20 @@ class SessionsController < ApplicationController
         else
             render json: { error: {login: "Invalid email or password"} }, status: :unauthorized
         end
+    end
+
+    def destroy
+        session.delete :account_id
+        head :no_content
+    end
+
+    private
+
+    def authorize
+        return render json: { errors: ["Not authorized"] }, status: :unauthorized unless session.include? :account_id
+    end
+    
+    def render_unprocessable_entity_response(invalid)
+        render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
     end
 end
