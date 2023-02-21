455b367e1f9e32762f7769ead84fc806af320042
Taylor <taylor@crucialartists.com>
2023-02-21 08:28:58 -0600

    Add dependent destroy to task_posts

--- app/models/employer.rb
+++ app/models/employer.rb
@@ -1,6 +1,6 @@
 class Employer < ApplicationRecord
     belongs_to :account
-    has_many :task_posts
+    has_many :task_posts, dependent: :destroy
     has_many :task_agreements, through: :task_posts
     has_many :assistants, -> { distinct }, through: :task_agreements
     has_many :reviews, through: :task_agreements
