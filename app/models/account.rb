2ceed289744f8baadc324e879f0966f8c0886710
Taylor <taylor@crucialartists.com>
2023-02-21 08:27:01 -0600

    Add dependent destroy to assistant and employer

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,9 +1,9 @@
 class Account < ApplicationRecord
     has_secure_password
 
-    has_one :assistant
-    has_one :employer
-    has_many :task_posts, through: :employer
+    has_one :assistant, dependent: :destroy
+    has_one :employer, dependent: :destroy
+    # has_many :task_posts, through: :employer
     # has_many :task_agreements, through: :task_posts
 
     validates :email, presence: true, uniqueness: true
