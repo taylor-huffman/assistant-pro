f5870cb8bc64fa2aa503f620ee75fa2fed13a6cf
Taylor <taylor@crucialartists.com>
2023-02-21 08:34:08 -0600

    Add has_one task_category association

--- app/serializers/task_agreement_serializer.rb
+++ app/serializers/task_agreement_serializer.rb
@@ -4,5 +4,6 @@ class TaskAgreementSerializer < ActiveModel::Serializer
   belongs_to :assistant
   belongs_to :employer
   belongs_to :task_post
+  has_one :task_category, through: :task_post
   has_one :review
 end
