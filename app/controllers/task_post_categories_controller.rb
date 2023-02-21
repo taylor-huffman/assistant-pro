1cdebfb11930b6b6a485e57430192bb5943aa306
Taylor <taylor@crucialartists.com>
2023-02-21 08:25:32 -0600

    Add show, create, update, and user_params

--- app/controllers/task_post_categories_controller.rb
+++ app/controllers/task_post_categories_controller.rb
@@ -5,4 +5,26 @@ class TaskPostCategoriesController < ApplicationController
         render json: task_post_categories
     end
 
+    def show
+        task_post_categories = TaskPostCategory.find(params[:id])
+        render json: task_post_categories
+    end
+
+    def create
+        task_post_categories = TaskPostCategory.create!(user_params)
+        render json: task_post_categories
+    end
+
+    def update
+        task_post_category = TaskPostCategory.find(params[:id])
+        task_post_category.update!(user_params)
+        render json: task_post_category
+    end
+
+    private
+
+    def user_params
+        params.permit(:task_post_id, :task_category_id)
+    end
+
 end
