8a3062d54bf29c7fe03991710e6136196c98bf73
Taylor <taylor@crucialartists.com>
2023-02-21 08:21:23 -0600

    Add update method

--- app/controllers/assistants_controller.rb
+++ app/controllers/assistants_controller.rb
@@ -10,4 +10,15 @@ class AssistantsController < ApplicationController
         render json: assistant
     end
 
+    def update
+        assistant = Assistant.find(params[:id])
+        assistant.update(
+            company_name: params[:company_name],
+            company_bio: params[:company_bio],
+            company_start_date: params[:company_start_date],
+            company_hourly_rate: params[:company_hourly_rate]
+        )
+        render json: assistant
+    end
+
 end
