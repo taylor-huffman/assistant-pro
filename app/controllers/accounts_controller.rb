f9cbda40d698b81fcd5b32dc5af7c6de4dd21660
Taylor <taylor@crucialartists.com>
2023-02-21 08:17:59 -0600

    Add authorize and destroy, modify create to include create session, modify show find params for testing frontend

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,5 +1,7 @@
 class AccountsController < ApplicationController
     rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+    before_action :authorize
+    skip_before_action :authorize, only: [:create, :index, :show]
 
     def index
         accounts = Account.all
@@ -8,19 +10,30 @@ class AccountsController < ApplicationController
     end
 
     def show
-        account = Account.find(session[:account_id])
-        render json: account, include: ['assistant', 'assistant.task_agreements', 'employer', 'employer.task_posts', 'employer.task_agreements', 'employer.assistants']
+        account = Account.find(params[:id])
+        render json: account, include: ['assistant', 'assistant.task_agreements', 'assistant.task_agreements.task_category', 'assistant.task_agreements.employer', 'employer', 'employer.task_posts', 'employer.task_posts.task_post_category', 'employer.task_posts.task_category', 'employer.task_agreements', 'employer.task_agreements.task_category', 'employer.reviews.task_post', 'employer.reviews.assistant', 'employer.assistants']
     end
 
     def create
         account = Account.create!(user_params)
+        session[:account_id] = account.id
         render json: account, include: ['assistant', 'assistant.task_agreements', 'employer', 'employer.task_posts', 'employer.task_agreements', 'employer.assistants']
+    end
+
+    def destroy
+        account = Account.find(params[:id])
+        account.destroy
+        head :no_content
     end
 
     private
 
-    def render_unprocessable_entity
+    def render_unprocessable_entity(invalid)
         render json: { error: invalid.record.errors }, status: :unprocessable_entity
+    end
+
+    def authorize
+        return render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :account_id
     end
 
     def user_params
