48dfea007b9e8f90702699b3fea4a459d2966c56
Taylor <taylor@crucialartists.com>
2023-02-21 08:24:13 -0600

    Add update and destroy

--- app/controllers/task_agreements_controller.rb
+++ app/controllers/task_agreements_controller.rb
@@ -10,4 +10,20 @@ class TaskAgreementsController < ApplicationController
         render json: task_agreement
     end
 
+    def update
+        task_agreement = TaskAgreement.find(params[:id])
+        task_agreement.update(
+            task_agreement_notes: params[:task_agreement_notes],
+            hourly_rate: params[:hourly_rate],
+            is_completed: params[:is_completed]
+        )
+        render json: task_agreement
+    end
+
+    def destroy
+        task_agreement = TaskAgreement.find(params[:id])
+        task_agreement.destroy
+        head :no_content
+    end
+
 end
