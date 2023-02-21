3b98647cba0d805735941f5c9c19f55accf1d65e
Taylor <taylor@crucialartists.com>
2023-02-21 08:26:09 -0600

    Add create, update, destroy, and user_params

--- app/controllers/task_posts_controller.rb
+++ app/controllers/task_posts_controller.rb
@@ -10,4 +10,31 @@ class TaskPostsController < ApplicationController
         render json: task_post
     end
 
+    def create
+        task_post = TaskPost.create!(user_params)
+        render json: task_post
+    end
+
+    def update
+        task_post = TaskPost.find(params[:id])
+        task_post.update!(
+            task_description: params[:task_description],
+            hourly_rate: params[:hourly_rate],
+            is_active: params[:is_active]
+        )
+        render json: task_post
+    end
+
+    def destroy
+        task_post = TaskPost.find(params[:id])
+        task_post.destroy
+        head :no_content
+    end
+
+    private
+
+    def user_params
+        params.permit(:employer_id, :task_description, :hourly_rate, :is_active)
+    end
+
 end
