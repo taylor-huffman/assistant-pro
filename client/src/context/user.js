fe681d0140f645bbdce2e54cfbac429da21cf362
Taylor <taylor@crucialartists.com>
2023-02-21 08:20:19 -0600

    Add auth and isLoading state, modify fetch route for testing

--- client/src/context/user.js
+++ client/src/context/user.js
@@ -4,26 +4,44 @@ const UserContext = React.createContext()
 
 function UserProvider({ children }) {
 
-    const [user, setUser] = useState(getUser)
+    const [user, setUser] = useState('')
+    const [isAuth, setIsAuth] = useState(false)
+    const [isLoading, setIsLoading] = useState(true)
 
-    // useEffect(() => {
-    //     fetch('/auth')
-    //     .then(r => r.json())
-    //     .then(user => {
-    //         setUser(user)
-    //     })
-    // }, [])
+    useEffect(() => {
+        fetch('/accounts/1')
+        .then(r => {
+            if (r.ok) {
+                r.json().then(user => {
+                    setUser(user)
+                    setIsAuth(true)
+                    setIsLoading(false)
+                })
+            } else {
+                r.json().then(error => {
+                    console.log(error)
+                    setIsLoading(false)
+                })
+            }
+        })
+    }, [])
 
-    function getUser() {
-        return fetch('/auth')
-            .then(r => r.json())
-            .then(user => {
-                setUser(user)
-            })
-    }
+    // function getUser() {
+    //     return fetch('/auth')
+    //         .then(r => {
+    //             if (r.ok) {
+    //                 r.json().then(user => {
+    //                     setUser(user)
+    //                     setIsAuth(true)
+    //                 })
+    //             } else {
+    //                 r.json().then(error => console.log(error))
+    //             }
+    //         })
+    // }
 
     return (
-        <UserContext.Provider value={{ user, setUser }}>{children}</UserContext.Provider>
+        <UserContext.Provider value={{ user, setUser, isAuth, setIsAuth, isLoading }}>{children}</UserContext.Provider>
     )
 }
 
