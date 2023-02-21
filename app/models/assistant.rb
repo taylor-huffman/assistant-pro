bf7bff232afc93583818e475157caf2e07179e2c
Taylor <taylor@crucialartists.com>
2023-02-21 08:28:16 -0600

    Change associations to has_one and add average_rating method

--- app/models/assistant.rb
+++ app/models/assistant.rb
@@ -2,6 +2,10 @@ class Assistant < ApplicationRecord
     belongs_to :account
     has_many :task_agreements
     has_many :reviews, through: :task_agreements
-    has_many :assistant_tasks
-    has_many :task_categories, through: :assistant_tasks
+    has_one :assistant_task
+    has_one :task_category, through: :assistant_task
+
+    def average_rating
+        self.reviews.average(:rating)
+    end
 end
