f6d236448416f6dc3c6fbe940e53db0ab989e0c4
Taylor <taylor@crucialartists.com>
2023-06-15 06:08:18 -0500

    Change account#show to find_by

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,5 +1,6 @@
 class AccountsController < ApplicationController
     rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
     before_action :authorize
     skip_before_action :authorize, only: [:create, :index, :show]
 
@@ -10,8 +11,12 @@ class AccountsController < ApplicationController
     end
 
     def show
-        account = Account.find(session[:account_id])
-        render json: account, include: ['assistant', 'assistant.task_agreements', 'assistant.task_agreements.task_category', 'assistant.task_agreements.employer', 'employer', 'employer.task_posts', 'employer.task_posts.task_post_category', 'employer.task_posts.task_category', 'employer.task_agreements', 'employer.task_agreements.task_category', 'employer.reviews.task_post', 'employer.reviews.assistant', 'employer.assistants', 'employer.assistants.account']
+        account = Account.find_by(id: session[:account_id])
+        if account
+            render json: account, include: ['assistant', 'assistant.task_agreements', 'assistant.task_agreements.task_category', 'assistant.task_agreements.employer', 'employer', 'employer.task_posts', 'employer.task_posts.task_post_category', 'employer.task_posts.task_category', 'employer.task_agreements', 'employer.task_agreements.task_category', 'employer.reviews.task_post', 'employer.reviews.assistant', 'employer.assistants', 'employer.assistants.account']
+        else
+            render json: { error: ["Sorry, you're not authorized"] }, status: :unauthorized
+        end 
     end
 
     def create
@@ -46,6 +51,10 @@ class AccountsController < ApplicationController
 
     def render_unprocessable_entity(invalid)
         render json: { error: invalid.record.errors.full_messages }, status: :unprocessable_entity
+    end
+
+    def render_not_found_response(error)
+        render json: { error: [error.message] }, status: :not_found
     end
 
     def authorize
